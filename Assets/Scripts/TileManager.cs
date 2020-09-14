5d3be3ae906707b67255bbf407ee7a6d8ae41d85
Romain DRIAT <romaindriat@gmail.com>
2020-09-14 16:40:18 +0200

    Script Folder

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public int size_x = 100;
+    public int size_y = 50;
+    public List<Sprite> tiles_sprite;
+    public Tile[,] board;
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = new Tile[size_x, size_y];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
