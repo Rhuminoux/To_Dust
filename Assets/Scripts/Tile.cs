7f89bb9e58d465e5e0ba3eada2a271b061b22131
Romain DRIAT <romaindriat@gmail.com>
2020-09-15 09:28:38 +0200

    Background  tiles

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public bool taken;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    public int size_x = 100;
-    public int size_y = 50;
+    public int size_x = 20;
+    public int size_y = 10;
     public List<Sprite> tiles_sprite;
     public Tile[,] board;
     // Start is called before the first frame update
@@ -18,5 +18,25 @@ public class TileManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public Sprite GetTileSprite(int x, int y)
+    {
+        return tiles_sprite[CheckAround(x, y)];
+    }
+
+    int CheckAround(int x, int y)
+    {
+        int result = 0;
+
+        if (x != 0 && board[x - 1, y].taken != true)
+            result += 8;
+        if (x != size_x - 1 && board[x + 1, y].taken != true)
+            result += 2;
+        if (y != 0 && board[x, y - 1].taken != true)
+            result += 1;
+        if (y != size_y - 1 && board[x, y + 1].taken != true)
+            result += 4;
+        return result;
     }
 }
