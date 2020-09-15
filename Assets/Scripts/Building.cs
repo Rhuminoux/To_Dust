173363728420b7319d4f24d127026ae0d160be19
Julien Brulle <jbrulle@dimosoftware.com>
2020-09-15 11:19:47 +0200

    Building / TimeDayNightManager / RessourcesManager

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public string S_name;
+    public int I_ressource = 1;
+
+    void Start()
+    {
+        TimeDayNightManager.TimePassed += AddRessourcesToStock_TimePassed;
+    }
+
+    public void AddRessourcesToStock_TimePassed(EventArgs e)
+    {
+        RessourcesManager.AddToStock(I_ressource);
+    }
+
+
+}
---
+++ Assets/Scripts/RessourcesManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RessourcesManager : MonoBehaviour
+{
+    public static int I_STOCK;
+    public int stock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stock = I_STOCK;
+    }
+
+    public static void AddToStock(int ressourcePoints)
+    {
+        I_STOCK += ressourcePoints;
+    }
+}
