c32f19e8e9e613471e376d42344e2070524780d6
Romain DRIAT <romaindriat@gmail.com>
2020-09-15 10:34:39 +0200

    Mp creator
    Création de la map en fonction des données entrées dans TileManager

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class Camera : MonoBehaviour
 {
-    public bool taken = false;
+    public GameObject tile_manager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(tile_manager.GetComponent<TileManager>().size_x / 2, tile_manager.GetComponent<TileManager>().size_y / 2, -10);    
     }
 
     // Update is called once per frame
--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -7,17 +7,48 @@ public class TileManager : MonoBehaviour
     public int size_x = 20;
     public int size_y = 10;
     public List<Sprite> tiles_sprite;
+    public List<GameObject> background_sprites;
+    public GameObject background_tiles_p;
+    public GameObject coral_tiles_p;
     public Tile[,] board;
+    public GameObject base_tile;
     // Start is called before the first frame update
     void Start()
     {
         board = new Tile[size_x, size_y];
+        CreateMap();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void CreateMap()
+    {
+        GameObject newTile;
+
+        for (int i = 0; i < size_x; ++i)
+        {
+            for (int j = 0; j < size_y; ++j)
+            {
+                if (j == 0)
+                {
+                    GameObject.Instantiate(background_sprites[0], new Vector3(i, j, 0), new Quaternion(0, 0, 0, 0), background_tiles_p.transform);
+                }
+                else
+                {
+                    GameObject.Instantiate(background_sprites[1], new Vector3(i, j, 0), new Quaternion(0, 0, 0, 0), background_tiles_p.transform);
+                }
+                newTile = GameObject.Instantiate(base_tile, new Vector3(i, j, 0), new Quaternion(0, 0, 0, 0), coral_tiles_p.transform);
+                board[i, j] = newTile.GetComponent<Tile>(); 
+            }
+        }
+        newTile = GameObject.Instantiate(base_tile, new Vector3(size_x / 2, 1, 0), new Quaternion(0, 0, 0, 0), coral_tiles_p.transform);
+        newTile.GetComponent<SpriteRenderer>().sprite = tiles_sprite[4];
+        newTile.GetComponent<Tile>().taken = true;
+        board[size_x / 2, size_y - 2] = newTile.GetComponent<Tile>();
     }
 
     public Sprite GetTileSprite(int x, int y)
@@ -33,9 +64,9 @@ public class TileManager : MonoBehaviour
             result += 8;
         if (x != size_x - 1 && board[x + 1, y].taken != true)
             result += 2;
-        if (y != 0 && board[x, y - 1].taken != true)
+        if (y != 0 && board[x, y + 1].taken != true)
             result += 1;
-        if (y != size_y - 1 && board[x, y + 1].taken != true)
+        if (y != size_y - 1 && board[x, y - 1].taken != true)
             result += 4;
         return result;
     }
