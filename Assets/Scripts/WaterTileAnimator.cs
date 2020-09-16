b06e3ea01004fbc8d494ae30fea0e63eb2acddd6
Romain DRIAT <romaindriat@gmail.com>
2020-09-16 15:51:02 +0200

    little changes

---
+++ Assets/Scripts/WaterTileAnimator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTileAnimator : MonoBehaviour
+{
+    public List<Sprite> sprites;
+    public float anim_time = 5f;
+
+    float timer;
+    bool current_frame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = anim_time;
+        current_frame = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            for (int i = 0; i < transform.childCount; ++i)
+            {
+                if (current_frame)
+                    transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+                else
+                    transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().sprite = sprites[1];
+                current_frame = !current_frame;
+            }
+            current_frame = !current_frame;
+            timer = anim_time;
+        }
+    }
+}
