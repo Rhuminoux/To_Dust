4e55cdf2168320a7fe30b99054388ef92abd8808
Julien Brulle <jbrulle@dimosoftware.com>
2020-09-21 13:22:39 +0200

    Enemy

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("Board Settings")]
+    public GameObject GO_tileManager;
+    public Tile[,] getBoard { get => GO_tileManager.GetComponent<TileManager>().board; }
+    public int getSizeX { get => GO_tileManager.GetComponent<TileManager>().size_x; }
+    public int getSizeY { get => GO_tileManager.GetComponent<TileManager>().size_y; }
+
+    private void Awake()
+    {
+        GO_tileManager = GameObject.FindWithTag("TileManager");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
